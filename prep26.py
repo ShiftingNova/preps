33dd529979cfc6975a6977ecde1e101c1d86f6f3
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-11-01 17:19:49 -0700

    Update prep26.py

--- prep26.py
+++ prep26.py
@@ -3,4 +3,7 @@ def differences(set1,set2):
     for i in set1:
         if i not in set2:
             total += 1
+    for i in set2:
+        if i not in set1:
+            total += 1
     return total
